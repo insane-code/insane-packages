0cc784fd4fb549e2f48e210f37f1a6d70b6da21e
jesusantguerrero <jesusantguerrero@users.noreply.github.com>
2022-07-14 23:35:17 +0000

    deploy: d4651e54f14b04b871d8aeacedc52aa79d5688f2

---
+++ assets/atmosphere_index.md.832af76c.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.0fb054e6.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"atmosphere/index.md"}'),r={name:"atmosphere/index.md"};function o(n,s,c,p,d,i){return a(),t("div")}var f=e(r,[["render",o]]);export{m as __pageData,f as default};
